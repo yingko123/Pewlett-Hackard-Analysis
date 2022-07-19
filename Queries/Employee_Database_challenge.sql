27303c84f829fcf06a6b5f812695c8ef8aedcdbb
yingko123 <yingko123@yahoo.com>
2022-07-19 14:37:30 -0500

    update

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -41,7 +41,7 @@ ORDER BY COUNT(title) DESC
 
 SELECT * FROM retiring_titles
 
--- 2. The Employees Eligible for the Mentorship Program
+-- 2. The Employees Eligible for the Mentorship Program (count=1549)
 SELECT DISTINCT ON (e.emp_no)
 	e.emp_no,
 	e.first_name,
@@ -60,3 +60,33 @@ WHERE de.to_date = ('9999-01-01')
 AND(e.birth_date BETWEEN '1965-01-01'AND '1965-12-31')
 
 SELECT * FROM mentorship_eligibility
+
+SELECT COUNT(emp_no) FROM mentorship_eligibility
+
+-- 3a. Expanding mentorship program (count=56,859)
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO expand_mentorship
+FROM employees as e
+	INNER JOIN dept_employees as de
+		ON(e.emp_no = de.emp_no)
+	INNER JOIN titles as ti
+		ON (de.emp_no = ti.emp_no)
+WHERE de.to_date = ('9999-01-01') 
+AND(e.birth_date BETWEEN '1962-01-01'AND '1967-12-31')
+
+SELECT * FROM expand_mentorship
+
+SELECT COUNT(emp_no) FROM expand_mentorship
+-- 3b. Expanded mentorship program by title cound
+SELECT count(title), title
+FROM expand_mentorship
+GROUP BY title
+ORDER BY COUNT(title) DESC
+
